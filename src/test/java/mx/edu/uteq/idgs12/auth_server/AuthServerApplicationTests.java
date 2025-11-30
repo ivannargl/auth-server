ae62b286c72117a1c634b997745851ccd76d6157
ivannargl <r.guzmanlopez89@gmail.com>
2025-11-30 16:18:53 -0600

    Estructura auth-server

---
+++ src/test/java/mx/edu/uteq/idgs12/auth_server/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs12.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
